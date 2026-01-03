7cbace3635c2e96d42b9518c30611578bc5550ae
VikasRajput-18 <vikasvikas98809@gmail.com>
2026-01-03 21:27:08 +0530

    fix issue

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,7 @@ import type { App } from "../app/api/[[...slugs]]/route";
 import { treaty } from "@elysiajs/eden";
 
 // .api to enter /api prefix
-export const client = treaty<App>("localhost:3000").api;
+export const client = treaty<App>(
+  process.env.NEXT_PUBLIC_API_BASE_URL!
+).api;
 
